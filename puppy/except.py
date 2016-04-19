ce71edd0075e2558806450e09f6b06ba623c013e
jakearthur22 <harmonicadude22@gmail.com>
2016-04-19 09:19:14 -0400

    Update except.py

--- puppy/except.py
+++ puppy/except.py
@@ -1,1 +1,20 @@
 #this will contain the code for the exception
+from gopigo import stop
+from gopigo import disable_servo
+from gopigo import disable_encoders
+
+def keyboard():
+  stop()
+  disable_servo()
+  disable_encoders()
+  print "Run ended! Keyboard Interrupt. \n Summary:"
+  print "Variables: \n DESIRED_DIST: " + str(DESIRED_DIST[0]) + "," + str(DESIRED_DIST[-1]) + "\n MAX_DIST: " + str(MAX_DIST) + "\n Current Distance: " + str(dist())
+
+def problem():
+  stop()
+  disable_servo()
+  disable_encoders()
+  print "Run ended!"
+  print str(type(e)) + ": " + str(e)
+  print "\n Summary:"
+  print "Variables: \n DESIRED_DIST: " + str(DESIRED_DIST[0]) + "," + str(DESIRED_DIST[-1]) + "\n MAX_DIST: " + str(MAX_DIST) + "\n Current Distance: " + str(dist())
