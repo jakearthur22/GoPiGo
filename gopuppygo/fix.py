a2832e75ae2456e72bb55b81784b5b08d555176f
jakearthur22 <harmonicadude22@gmail.com>
2016-05-02 04:09:23 -0400

    Update and rename puppy/fix.py to gopuppygo/fix.py
    Final upload of the code.

--- gopuppygo/fix.py
+++ gopuppygo/fix.py
@@ -1,5 +1,9 @@
 #this will contain the code for the exception
-from puppy.func import kill
+from func import kill
+from func import dist
+from func import DESIRED_DIST
+from func import MAX_DIST
+from gopigo import us_dist
 
 def keyboard():
   kill()
@@ -12,3 +16,4 @@ def problem():
   print str(type(e)) + ": " + str(e)
   print "\n Summary:"
   print "Variables: \n DESIRED_DIST: " + str(DESIRED_DIST[0]) + "," + str(DESIRED_DIST[-1]) + "\n MAX_DIST: " + str(MAX_DIST) + "\n Current Distance: " + str(dist())
+  
