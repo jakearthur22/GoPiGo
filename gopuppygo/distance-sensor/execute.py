c7e5fd4e45bfdb5d9665a16d2f93a1cdc0325951
jakearthur22 <harmonicadude22@gmail.com>
2016-05-03 09:52:05 -0400

    Update execute.py
    update the code as a function to make it easier to re-run, without having to hook up to a monitor.

--- gopuppygo/distance-sensor/execute.py
+++ gopuppygo/distance-sensor/execute.py
@@ -2,16 +2,19 @@
 import func
 import fix
 
-try:
-    print "Booting..."
-    func.reset()
-    print "Servo setup complete."
-    func.dance()
-    print "Dance sequence complete."
-    func.run()
-except KeyboardInterrupt:
-    fix.keyboard()
-except Exception as e:
-    fix.problem()
-except Exception:
-    print "Error importing. Code needs maintenance."
+def ex():
+    try:
+        print "Booting..."
+        func.reset()
+        print "Servo setup complete."
+        func.dance()
+        print "Dance sequence complete."
+        func.run()
+    except KeyboardInterrupt:
+        fix.keyboard()
+    except Exception as e:
+        fix.problem()
+    except Exception:
+        print "Error importing. Code needs maintenance."
+
+ex()
