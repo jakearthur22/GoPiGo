53b78a5e644e1e9ebd2f47f5c3d959061bbb77ea
jakearthur22 <harmonicadude22@gmail.com>
2016-05-03 10:01:32 -0400

    Update execute.py
    Modified the ImportError exception.

--- gopuppygo/distance-sensor/execute.py
+++ gopuppygo/distance-sensor/execute.py
@@ -14,7 +14,9 @@ def ex():
         fix.keyboard()
     except Exception as e:
         fix.problem()
-    except Exception:
-        print "Error importing. Code needs maintenance."
+    except ImportError:
+        print "Files not installed correctly."
+        print "Please make sure to install all files and keep"
+        print "them in the 'gopuppygo' folder."
 
 ex()
